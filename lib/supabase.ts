34f0093cb403995ac0b878af452f5d9145f4fbd2
Ermal1 <ermal_aliu10@hotmail.com>
2025-12-04 14:01:28 +0100

    supabase conenction

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js"
 import type { Database } from "./database.types"
 
-const supabaseUrl = "https://zhmisoijuhzviasuvkwd.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpobWlzb2lqdWh6dmlhc3V2a3dkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2OTg2NTUsImV4cCI6MjA2ODI3NDY1NX0.cPKo3nAYNKCiEM3heezDZd92xr-7UWwEccrj9rs3jVQ"
+const supabaseUrl = "https://skezrqipdsdzviclhahw.supabase.co"  
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNrZXpycWlwZHNkenZpY2xoYWh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3OTg0MTMsImV4cCI6MjA4MDM3NDQxM30.DTHS8UeVZeG_S45z0vnwwT_R9tVP0LCUnVJEKsYsj5s"
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   realtime: {
